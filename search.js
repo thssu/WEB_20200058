a18a8037b45b5d7f8927b04545333d6eb234dfe3
손수민 <ssmstar@sungkyul.ac.kr>
2023-03-29 02:11:32 +0000

    4주차 실습

---
+++ search.js
@@ -0,0 +1,10 @@
+<head>
+<script type="text/javascript" src="js/search.js" defer></script>
+
+</head>
+
+document.getElementById("search_btn").addEventListener('click', search_message);
+
+function search_message(){
+   alert("검색을 수행합니다!"); 
+}
