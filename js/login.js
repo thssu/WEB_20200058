3bcc3ca2fe94fdd052ff322543f17731dd10d504
손수민 <ssmstar@sungkyul.ac.kr>
2023-05-17 02:30:48 +0000

    11주차 ppt 15페이지 까지

--- js/login.js
+++ js/login.js
@@ -1,19 +1,31 @@
-function login(){ //로그인
-	let form = document.querySelector("#form_main");
+function login(){ // 로그인
+    let form = document.querySelector("#form_main");
     let id = document.querySelector("#floatingInput");
     let password = document.querySelector("#floatingPassword");
-	
-	form.action = "../index_login.html";
-    form.method = "get"
+    let check = document.querySelector("#idSaveCheck");
+    
+    form.action = "../index_login.html";
+    form.method = "get";
+    
+    if(check.checked == true) { // 아이디 체크 o
+            alert("쿠키를 저장합니다.");
+            setCookie("id", id.value, 1); // 1일 저장
+            alert("쿠키 값 :" + id.value);
+        } 
+    else { // 아이디 체크 x
+            setCookie("id", id.value, 0); //날짜를 0 - 쿠키 삭제
+    }
 	
 	if(id.value.length === 0 || password.value.length === 0){
         alert("아이디와 비밀번호를 모두 입력해주세요.")
     }else{
+		session_set(); // 세션 생성
         form.submit();
     }
 }
 
 function logout(){ //로그아웃
+	session_del(); // 세션 삭제
     location.href='../index.html';
 }
 
@@ -32,6 +44,78 @@ function get_id(){
 		    }
 	    } // 2중 for문 끝
 }; // 함수 끝
+function deleteCookie(cookieName){
+    var expireDate = new Date();
+    expireDate.setDate(expireDate.getDate() - 1);
+    document.cookie = cookieName + "= " + "; expires=" + expireDate.toGMTString();
+}
+function init(){ // 로그인 폼에 쿠키에서 가져온 아이디 입력
+    let id = document.querySelector("#floatingInput");
+    let check = document.querySelector("#idSaveCheck");
+    let get_id = getCookie("id");
+    
+    if(get_id) { 
+    id.value = get_id; 
+    check.checked = true; 
+    }
+	session_check(); // 세션 유무 검사
+}
+// 로그인 횟수를 증가시키고 쿠키를 업데이트하는 함수_10주차 응용문제
+function login_count() {
+    var count = getCookie("login_cnt");
+    if (count) {
+        count = parseInt(count) + 1;
+    } else {
+        count = 1;
+    }
+    setCookie("login_cnt", count, 365); // 1년 동안 유지되는 쿠키 설정
+}
+
+// 로그아웃 횟수를 증가시키고 쿠키를 업데이트하는 함수_10주차 응용문제
+function logout_count() {
+    var count = getCookie("logout_cnt");
+    if (count) {
+        count = parseInt(count) + 1;
+    } else {
+        count = 1;
+    }
+    setCookie("logout_cnt", count, 365); // 1년 동안 유지되는 쿠키 설정
+}
+function session_set() { //세션 저장
+    let id = document.querySelector("#floatingInput");
+    if (sessionStorage) {
+        sessionStorage.setItem("Session_Storage_test", id.value);
+
+    } else {
+        alert("로컬 스토리지 지원 x");
+    }
+}
+function session_get() { //세션 읽기
+    if (sessionStorage) {
+       return sessionStorage.getItem("Session_Storage_test");
+    } else {
+        alert("세션 스토리지 지원 x");
+    }
+}
+function session_check() { //세션 검사
+    if (sessionStorage.getItem("Session_Storage_test")) {
+        alert("이미 로그인 되었습니다.");
+        location.href='index_login.html'; // 로그인된 페이지로 이동
+    }
+}
+function session_del() {//세션 삭제
+    // Check if the sessionStorage object exists
+    if (sessionStorage) {
+        // Retrieve data
+        sessionStorage.removeItem("Session_Storage_test");
+        alert('로그아웃 버튼 클릭 확인 : 세션 스토리지를 삭제합니다.');
+    } else {
+        alert("세션 스토리지 지원 x");
+    }
+}
+
+
+	
 alert(getParameters('id') + '님 환영합니다!'); // 메시지 창 출력
 }
 
--- js/pop_up.js
+++ js/pop_up.js
@@ -1,6 +1,10 @@
-function pop_up(){
-	window.open("../pop_up/pop_up.html","팝업테스트","width=400, height=300, top=10, left=10");
+function pop_up() {
+	var cookieCheck = getCookie("popupYN");
+        if (cookieCheck != "N"){
+        window.open("pop_up.html", "팝업테스트", "width=400, height=300, top=10, left=10");
+        }
 }
+
 function show_clock(){
         let currentDate = new Date(); // 날짜 객체 생성
         let divClock = document.getElementById('divClock');
@@ -23,3 +27,31 @@ function show_clock(){
         }
         setTimeout(show_clock, 1000);  //1초마다 갱신
 }
+function setCookie(name, value, expiredays) {
+        var date = new Date();
+        date.setDate(date.getDate() + expiredays);
+        document.cookie = escape(name) + "=" + escape(value) + "; expires=" + date.toUTCString() + "SameSite=None; Secure";        
+}
+function getCookie(name) {
+        var cookie = document.cookie;
+        console.log("쿠키를 요청합니다.");
+        if (cookie != "") {
+            var cookie_array = cookie.split("; ");
+            for ( var index in cookie_array) {
+                var cookie_name = cookie_array[index].split("=");
+                
+                if (cookie_name[0] == "popupYN") {
+                    return cookie_name[1];
+                }
+            }
+        }
+        return ;
+}
+function closePopup() {
+        if (document.getElementById('check_popup').value) {
+            setCookie("popupYN", "N", 1);
+            console.log("쿠키를 설정합니다.");
+            self.close();
+        }
+    }
+
