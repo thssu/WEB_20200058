058571a2d2d5c5b0fee1e54796c5a5e8235bd450
손수민 <ssmstar@sungkyul.ac.kr>
2023-05-24 02:29:04 +0000

    12주차 실습내용

---
+++ js/cookie.js
@@ -0,0 +1,5 @@
+function deleteCookie(cookieName){
+    var expireDate = new Date();
+    expireDate.setDate(expireDate.getDate() - 1);
+    document.cookie = cookieName + "= " + "; expires=" + expireDate.toGMTString();
+}
--- js/login.js
+++ js/login.js
@@ -1,3 +1,24 @@
+function addJavascript(jsname) { // 자바스크립트 외부 연동
+	var th = document.getElementsByTagName('head')[0];
+	var s = document.createElement('script');
+	s.setAttribute('type','text/javascript');
+	s.setAttribute('src',jsname);
+	th.appendChild(s);
+}
+addJavascript('/js/security.js'); // 암복호화 함수
+addJavascript('/js/session.js'); // 세션 함수
+addJavascript('/js/cookie.js'); // 쿠키 함수	
+
+function init(){ // 로그인 폼에 쿠키에서 가져온 아이디 입력
+    let id = document.querySelector("#floatingInput");
+    let check = document.querySelector("#idSaveCheck");
+    let get_id = getCookie("id");
+    
+    if(get_id) {
+    }
+	session_check(); // 세션 유무 검사
+}
+
 function login(){ // 로그인
     let form = document.querySelector("#form_main");
     let id = document.querySelector("#floatingInput");
@@ -30,6 +51,10 @@ function logout(){ //로그아웃
 }
 
 function get_id(){
+	if(true){
+        decrypt_text();
+    }
+    else{
     var getParameters = function(paramName){ // 변수 = 함수(이름)
     var returnValue; // 리턴값을 위한 변수 선언
     var url = location.href; // 현재 접속 중인 주소 정보 저장
@@ -44,22 +69,10 @@ function get_id(){
 		    }
 	    } // 2중 for문 끝
 }; // 함수 끝
-function deleteCookie(cookieName){
-    var expireDate = new Date();
-    expireDate.setDate(expireDate.getDate() - 1);
-    document.cookie = cookieName + "= " + "; expires=" + expireDate.toGMTString();
+	alert(getParameters('id') + '님 환영합니다!'); // 메시지 창 출력
 }
-function init(){ // 로그인 폼에 쿠키에서 가져온 아이디 입력
-    let id = document.querySelector("#floatingInput");
-    let check = document.querySelector("#idSaveCheck");
-    let get_id = getCookie("id");
-    
-    if(get_id) { 
-    id.value = get_id; 
-    check.checked = true; 
-    }
-	session_check(); // 세션 유무 검사
 }
+
 // 로그인 횟수를 증가시키고 쿠키를 업데이트하는 함수_10주차 응용문제
 function login_count() {
     var count = getCookie("login_cnt");
@@ -81,42 +94,3 @@ function logout_count() {
     }
     setCookie("logout_cnt", count, 365); // 1년 동안 유지되는 쿠키 설정
 }
-function session_set() { //세션 저장
-    let id = document.querySelector("#floatingInput");
-    if (sessionStorage) {
-        sessionStorage.setItem("Session_Storage_test", id.value);
-
-    } else {
-        alert("로컬 스토리지 지원 x");
-    }
-}
-function session_get() { //세션 읽기
-    if (sessionStorage) {
-       return sessionStorage.getItem("Session_Storage_test");
-    } else {
-        alert("세션 스토리지 지원 x");
-    }
-}
-function session_check() { //세션 검사
-    if (sessionStorage.getItem("Session_Storage_test")) {
-        alert("이미 로그인 되었습니다.");
-        location.href='index_login.html'; // 로그인된 페이지로 이동
-    }
-}
-function session_del() {//세션 삭제
-    // Check if the sessionStorage object exists
-    if (sessionStorage) {
-        // Retrieve data
-        sessionStorage.removeItem("Session_Storage_test");
-        alert('로그아웃 버튼 클릭 확인 : 세션 스토리지를 삭제합니다.');
-    } else {
-        alert("세션 스토리지 지원 x");
-    }
-}
-
-
-	
-alert(getParameters('id') + '님 환영합니다!'); // 메시지 창 출력
-}
-
-	
